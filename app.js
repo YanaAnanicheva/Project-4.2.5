c190ec23aef0a61082810aa6e5c1afee4115f602
YanaAnanicheva <ananichevayana@gmail.com>
2022-03-02 13:27:48 +0300

    Внесены правки Project 4.2.5

--- app.js
+++ app.js
@@ -14,17 +14,19 @@ async function receivedRequest() {
     .then((result) => {
         result.json()
         .then(result => {
-          searchResult(result, enteredRequest)
-          enteredRequest.onclick = (ev) => createChekedRepo(ev, result)
+          searchResult(result, enteredRequest);
+          enteredRequest.onclick = (ev) => { createCheckedRepo(ev, result);
+            deleteOldSearchResult(enteredRequest);
+          }
         })
       })
     }
 }
 
 function searchResult(foundRepo, enteredRequest) {
-  while(enteredRequest.children.length != 0) {
+  while(enteredRequest.children.length !== 0) {
     enteredRequest.removeChild(enteredRequest.firstChild)
-  } 
+  }
   createResSearchResult(foundRepo, enteredRequest)
 }
 
@@ -32,18 +34,20 @@ function createResSearchResult(foundRepo, repoList) {
   let reposResult = 0;
   foundRepo.items.length >= 5 ? reposResult  = 5 : reposResult = foundRepo.items.length;
 
+  let newRepo;
+
   for (let i = 0; i < reposResult; i++){
-    let newRepo = document.createElement('div');
+    newRepo = document.createElement('div');
     newRepo.classList.add('main-background__search-result');
     newRepo.textContent = foundRepo.items[i].name;
     repoList.appendChild(newRepo);
   }
 }
 
-function createChekedRepo(ev, result) {
+function createCheckedRepo(ev, result) {
   ev.stopPropagation()
   let repoName = ev.target.innerText;
-  let [chekedRepo] = result.items.filter(function (repo) {
+  let [checkedRepo] = result.items.filter(function (repo) {
     return repoName === repo.name;
   })
 
@@ -52,15 +56,15 @@ function createChekedRepo(ev, result) {
   repoList.appendChild(repoItem);
 
   let repoNameText = document.createElement('p');
-  repoNameText.textContent = `Name: ${chekedRepo.name}`;
+  repoNameText.textContent = `Name: ${checkedRepo.name}`;
   repoItem.appendChild(repoNameText);
 
   let repoOwnerText = document.createElement('p');
-  repoOwnerText.textContent = `Owner: ${chekedRepo.owner.login}`;
+  repoOwnerText.textContent = `Owner: ${checkedRepo.owner.login}`;
   repoNameText.appendChild(repoOwnerText);
 
   let repoStarsText = document.createElement('p');
-  repoStarsText.textContent = `Stars: ${chekedRepo.stargazers_count}`
+  repoStarsText.textContent = `Stars: ${checkedRepo.stargazers_count}`
   repoOwnerText.appendChild(repoStarsText)
 
   let repoCloseBtn = document.createElement('div');
@@ -72,8 +76,10 @@ function createChekedRepo(ev, result) {
   input.value = '';
 }
 
-if(repoList.children.length === 0) {
-  enteredRequest.innerHTML = ''
+function deleteOldSearchResult(enteredRequest) {
+  while(enteredRequest.children.length !== 0) {
+    enteredRequest.removeChild(enteredRequest.firstChild)
+  }
 }
 
 function debounce (cb, delay){
